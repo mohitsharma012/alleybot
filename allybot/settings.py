7020dc78bdde7d691de7ea56226e08d92edba64b
mohitsharma012 <stayer.mohit@gmail.com>
2024-02-09 14:57:05 +0530

    now this is ready final 3

--- allybot/settings.py
+++ allybot/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = 'allybot.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['template'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -139,7 +139,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
